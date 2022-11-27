49467a5157e9778c88edbbf6db83321fc35636f2
js819park <js819park@gmail.com>
2022-11-27 12:39:28 +0900

    HDS-30 배포를 위한 test 수정

--- fitInserver/src/test/java/fitIn/fitInserver/FitInserverApplicationTests.java
+++ fitInserver/src/test/java/fitIn/fitInserver/FitInserverApplicationTests.java
@@ -1,13 +1,32 @@
-package fitIn.fitInserver;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class FitInserverApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package fitIn.fitInserver;
+//
+//import fitIn.fitInserver.controller.HelloController;
+//import org.junit.jupiter.api.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//import org.springframework.test.web.servlet.MockMvc;
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+//
+//
+//@RunWith(SpringRunner.class)
+//@WebMvcTest(controllers = HelloController.class)
+//class FitInserverApplicationTests {
+//
+//	@Autowired
+//	private MockMvc mvc;
+//
+//	@Test
+//	public void hello가_리턴된다() throws Exception{
+//		String hello = "hello";
+//
+//		mvc.perform(get("/hello"))
+//				.andExpect(status().isOk())
+//				.andExpect(content().string(hello));
+//	}
+//
+//}
